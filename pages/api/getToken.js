2114c80c91048a4e618632089f5d3c75c5fd0590
Nander <nander@nander.dev>
2020-09-20 18:19:10 +0000

    :art: make api requests with promise

--- pages/api/getToken.js
+++ pages/api/getToken.js
@@ -1,30 +1,34 @@
 import btoa from "btoa";
 
 export default (req, res) => {
-  const code = req.query.code;
-  const grant_type = "authorization_code";
-  const redirect_uri = process.env.NEXT_PUBLIC_REDIRECT_URI;
-  if (!code) {
-    res.status(404).json({
-      error: "Missing code",
-    });
-  }
-  fetch(`https://accounts.spotify.com/api/token`, {
-    headers: {
-      'Authorization':
-        "Basic " +
-        btoa(
-          process.env.NEXT_PUBLIC_CLIENT_ID + ":" + process.env.CLIENT_SECRET
-        ),
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      code: code,
-      grant_type: grant_type,
-      redirect_uri: redirect_uri,
-    }),
-    method: "POST",
-  })
-    .then((response) => response.json())
-    .then((response) => res.status(200).json(response));
+  return new Promise((resolve) => {
+    const code = req.query.code;
+    const grant_type = "authorization_code";
+    const redirect_uri = process.env.NEXT_PUBLIC_REDIRECT_URI;
+    if (!code) {
+      res.status(404).send({});
+      resolve();
+    }
+    fetch(`https://accounts.spotify.com/api/token`, {
+      headers: {
+        Authorization:
+          "Basic " +
+          btoa(
+            process.env.NEXT_PUBLIC_CLIENT_ID + ":" + process.env.CLIENT_SECRET
+          ),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        code: code,
+        grant_type: grant_type,
+        redirect_uri: redirect_uri,
+      }),
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        res.status(200).json(response);
+        resolve();
+      });
+  });
 };
