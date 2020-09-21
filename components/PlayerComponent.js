206bd37ef2ae4be18da51f837ac896989926e5f9
Nander <nander@nander.dev>
2020-09-21 19:37:59 +0000

    :construction started PlayerComponent

--- components/PlayerComponent.js
+++ components/PlayerComponent.js
@@ -1,12 +1,16 @@
-import * as styles from "../styles/Player.module.scss";
+import styles from "../styles/Player.module.scss";
+import useSWR from 'swr';
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function PlayerComponent(props) {
   const access_token = props.access_token;
+  const { data, error } = useSWR('') // WIP TODO 
 
   return (
     <div>
       <main className={styles.main}>
-        <p>hi</p>
+        
       </main>
     </div>
   );
