993426d1441cded88420393d8f5bbc0e6bf11084
Nander <nander@nander.dev>
2020-09-21 15:10:28 +0000

    :lipstick: use SASS instead of CSS

--- components/PlayerComponent.js
+++ components/PlayerComponent.js
@@ -1,3 +1,13 @@
-export default function PlayerComponent() {
-    return (<div></div>)
+import * as styles from "../styles/Player.module.scss";
+
+export default function PlayerComponent(props) {
+  const access_token = props.access_token;
+
+  return (
+    <div>
+      <main className={styles.main}>
+        <p>hi</p>
+      </main>
+    </div>
+  );
 }
